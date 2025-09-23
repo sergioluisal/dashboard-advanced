668efa98bad311aa5132da29f2fc47ee06a70855
sergioluisal <sergioluisal@gmail.com>
2025-09-23 13:00:20 -0300

    Update dashboard_home.py

--- dashboard_home.py
+++ dashboard_home.py
@@ -261,5 +261,5 @@ server = app.server  # importante para o Render reconhecer
 app.layout = html.Div("Hello Render!")
 
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=10000)
 
